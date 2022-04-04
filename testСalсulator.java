7be1bdc0d0007644788ed967dfc65db26558f914
unknown <anenkova33@gmail.com>
2022-04-05 01:25:37 +0300

    testСalсulator.java

---
+++ testСalсulator.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+
+        char sym; double answ = 0;
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Первое число: ");
+        double num1 = in.nextDouble();
+
+        System.out.print("Действие: ");
+        sym = (char) System.in.read();
+
+        System.out.print("Второе число: ");
+        double num2 = in.nextDouble();
+
+
+
+
+        switch (sym) {
+            case '+': answ =    num1 + num2;
+                break;
+            case '-':
+                answ = num1 - num2;
+                break;
+            case '*':
+                answ = num1 * num2;
+                break;
+            case '/':
+                answ = num1 / num2;
+                break;
+            default:
+                System.out.println("Ooops");
+
+        }
+        System.out.println("Результат: " + answ);
+
+
+
+    }
+
+}
